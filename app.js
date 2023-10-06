c97227952ce42d9688920a1749b8e867eb87c9b6
Charly66munoz <agustinolopezz8@gmail.com>
2023-10-06 14:21:30 +0200

    logo upload & some modification on public and app.js

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const app= express();
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.get('/',(req,res) =>{
+    res.send("Base-1");
+});
+
+app.get('/mostrat_sitio', (req,res) =>{
+    res.sendFile((__dirname+'/views/home.html'))
+});
+
 app.listen(3030, function() {
     console.log("Servidor corriendo");
 });
+
